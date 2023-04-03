43e06f216102fd58cd005899ca88848caeb23053
Lysanleo <lysanleo347@outlook.com>
2023-04-03 23:21:56 +0800

    Finished compiler of remove_complex_operands.

--- compiler.py
+++ compiler.py
@@ -15,67 +15,80 @@ class Compiler:
     # Remove Complex Operands
     ############################################################################
 
-    def rco_exp(self, e: expr, need_atomic : bool) -> Tuple[expr, Temporaries]:
+    def rco_flat(self, rcolist: Tuple[expr, Temporaries]) -> List[stmt]:
+        stmts = [Assign([atmtp[0]], atmtp[1]) for atmtp in rcolist[1]]
+        print(stmts)
+        return stmts
+
+    def rco_exp(self, e: expr, need_atomic: bool) -> Tuple[expr, Temporaries]:
+        new_sym = ""
         match e:
-          case  Constant(n):
-            return Constant(n), []
-          case Call(Name('input_int'),[]):
-            if need_atomic:
-              tmp = Name(generate_name('tmp'))
-              return tmp, [(tmp, Call(Name('input_int'),[]))]
-            else:
-              return Call(Name('input_int'),[]), []
-          case UnaryOp(USub(), exp):
-             (atm, temps) = self.rco_exp(exp, True)
-             usub = UnaryOp(USub(), atm)
-             if need_atomic:
-               tmp = Name(generate_name('tmp'))
-               return tmp, temps + [(tmp, usub)]
-             else:
-               return usub, temps
-          case BinOp(exp1, Add(), exp2):
-             (atm1, temps1) = self.rco_exp(exp1, True)
-             (atm2, temps2) = self.rco_exp(exp2, True)
-             add = BinOp(atm1, Add(), atm2)
-             if need_atomic:
-               tmp = Name(generate_name('tmp'))
-               return tmp, temps1 + temps2 + [(tmp, add)]
-             else:
-               return add, temps1 + temps2
-          case BinOp(exp1, Sub(), exp2):
-             (atm1, temps1) = self.rco_exp(exp1, True)
-             (atm2, temps2) = self.rco_exp(exp2, True)
-             sub = BinOp(atm1, Sub(), atm2)
-             if need_atomic:
-               tmp = Name(generate_name('tmp'))
-               return tmp, temps1 + temps2 + [(tmp, sub)]
-             else:
-               return sub, temps1 + temps2
-          case Name(var):
-             return Name(var), []
-          case _:
-            raise Exception('rco_exp unexpected ' + repr(e))
+            case Constant(value):
+                return (e, [])
+            case Name(id):
+                return (e, [(e, e)])
+            case BinOp(latm, Add(), ratm):
+                atm1 = self.rco_exp(latm, True)
+                atm2 = self.rco_exp(ratm, True)
+                new_expr = BinOp(atm1[0], Add(), atm2[0])
+                new_bindings = atm1[1] + atm2[1]
+                print(new_bindings)
+                if need_atomic:
+                    new_sym = generate_name("tmp")
+                    return (Name(new_sym), new_bindings + [(Name(new_sym), new_expr)])
+                return (new_expr, new_bindings)
+            case BinOp(latm, Sub(), ratm):
+                atm1 = self.rco_exp(latm, True)
+                atm2 = self.rco_exp(ratm, True)
+                new_expr = BinOp(atm1[0], Sub(), atm2[0])
+                new_bindings = atm1[1] + atm2[1]
+                if need_atomic:
+                    new_sym = generate_name("tmp")
+                    return (Name(new_sym), new_bindings + [([Name(new_sym)], new_expr)])
+                return (new_expr, new_bindings)
+            case UnaryOp(USub(), atm):
+                atm1 = self.rco_exp(atm, True)
+                new_expr = UnaryOp(USub(), atm1[0])
+                new_bindings = atm1[1]
+                if need_atomic:
+                    new_sym = generate_name("tmp")
+                    return (Name(new_sym), new_bindings + [(Name(new_sym), new_expr)])
+                return (new_expr, new_bindings)
+            case Call(Name('input_int'), []):
+                return (e, [])
 
     def rco_stmt(self, s: stmt) -> List[stmt]:
-      match s:
-        case Expr(Call(Name('print'), [exp])):
-           (atm, temps) = self.rco_exp(exp, True)
-           return [Assign([var], init) for (var,init) in temps] + [Expr(Call(Name('print'), [atm]))]
-        case Expr(exp) :
-           (atm, temps) = self.rco_exp(exp, False)
-           return [Assign([var], init) for (var,init) in temps]
-        case Assign([Name(var)], exp):
-           (atm, temps) = self.rco_exp(exp, False)
-           return [Assign([x], init) for (x,init) in temps] + [Assign([Name(var)], atm)]
-        case _:
-          raise Exception('rco_stmt not implemented')
+        match s:
+            case Expr(Call(Name('print'), [atom])):
+                # print(atom)
+                rcotp = self.rco_exp(atom, True)
+                # print(rcotp)
+                new_exprs = self.rco_flat(rcotp)
+                new_exprs.append(Expr(Call(Name('print'), [rcotp[0]])))
+                # print(new_exprs)
+                return new_exprs
+            case Expr(exp):
+                rcotp = self.rco_exp(exp, False)
+                new_exprs = self.rco_flat(rcotp)
+                new_exprs.append(Expr(rcotp[0]))
+                return new_exprs
+            case Assign([Name(var)], exp):
+                rcotp = self.rco_exp(exp, False)
+                new_exprs = self.rco_flat(rcotp)
+                new_exprs.append(Assign([Name(var)], rcotp[0]))
+                return new_exprs
 
     def remove_complex_operands(self, p: Module) -> Module:
         match p:
-          case Module(ss):
-            sss = [self.rco_stmt(s) for s in ss]
-            return Module(sum(sss, []))
-        raise Exception('remove_complex_operands not implemented')
+            case Module(body):
+                print(body)
+                new_body = []
+                new_body_temp = [self.rco_stmt(s) for s in body]
+                # print(new_body_temp == [None]) => True
+                for stmts in new_body_temp:
+                    new_body = new_body + stmts
+                print(new_body)
+                return Module(new_body)
         
 
     ############################################################################
