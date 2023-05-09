a625a9c0689b5fd03884dba315f04862b0a676f1
Lysanleo <lysanleo347@outlook.com>
2023-05-09 09:44:48 +0800

    Add tests for Lif.

--- run-tests.py
+++ run-tests.py
@@ -2,10 +2,14 @@ import os
 import compiler
 import interp_Lvar
 import type_check_Lvar
-from utils import run_tests, run_one_test
+import interp_Lif
+import type_check_Lif
+from utils import enable_tracing, run_tests, run_one_test
 from interp_x86.eval_x86 import interp_x86
 
 compiler = compiler.Compiler()
+
+# Test Options for Lvar
 
 typecheck_Lvar = type_check_Lvar.TypeCheckLvar().type_check
 
@@ -21,6 +25,22 @@ interp_dict = {
     'patch_instructions': interp_x86,
 }
 
+
+# Test Options for Lif
+
+typecheckLif = type_check_Lif.TypeCheckLif().type_check
+interpLif = interp_Lif.InterpLif().interp
+
+typecheck_if_dict = {
+    'source': typecheckLif,
+    'shrink': typecheckLif,
+    'remove_complex_operands': typecheckLif,
+}
+interp_if_dict = {
+    'shrink': interpLif,
+    'remove_complex_operands': interpLif,
+}
+
 if False:
     run_one_test(os.getcwd() + '/tests/var/zero.py',
                  'var',
@@ -29,7 +49,11 @@ if False:
                  typecheck_dict,
                  interp_dict)
 else:
+    enable_tracing()
     run_tests('var', compiler, 'var',
               typecheck_dict,
               interp_dict)
 
+    run_tests('if', compiler, 'if',
+              typecheck_if_dict,
+              interp_if_dict)
