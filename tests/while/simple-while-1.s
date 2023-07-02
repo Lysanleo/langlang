16717a957c3e6f308cbfaaf959ecfdffceb36849
Lysanleo <lysanleo347@outlook.com>
2023-07-02 11:02:40 +0800

    Fix compute_la, build_interference, block oreder.

--- tests/while/simple-while-1.s
+++ tests/while/simple-while-1.s
@@ -1,39 +1,43 @@
 	.align 16
-block.0:
-    cmpq $0, %rdx
-    jg block.2
-    jmp block.1
+block.18:
+    cmpq $0, %rcx
+    jg block.20
+    jmp block.19
 
 	.align 16
-block.1:
-    movq %rcx, %rdi
+block.19:
+    movq %rdx, %rdi
     callq print_int
     movq $0, %rax
     jmp conclusion
 
 	.align 16
-block.2:
-    addq %rdx, %rcx
-    subq $1, %rdx
-    jmp block.0
+block.20:
+    addq %rcx, %rdx
+    subq $1, %rcx
+    jmp block.18
 
 	.align 16
 start:
-    movq $5, %rdx
-    movq $0, %rcx
-    jmp block.0
+    movq $5, %rcx
+    movq $0, %rdx
+    jmp block.18
 
 	.globl main
 	.align 16
 main:
     pushq %rbp
     movq %rsp, %rbp
+    pushq %r12
+    pushq %r13
     subq $0, %rsp
     jmp start
 
 	.align 16
 conclusion:
     addq $0, %rsp
+    popq %r12
+    popq %r13
     popq %rbp
     retq
 
