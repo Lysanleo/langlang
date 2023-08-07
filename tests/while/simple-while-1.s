9e4fb0055cc7c37e994e8d2b48e4bef110018127
Lysanleo <lysanleo347@outlook.com>
2023-08-08 00:58:06 +0800

    Finished tuple expose allocation pass.

--- tests/while/simple-while-1.s
+++ tests/while/simple-while-1.s
@@ -1,26 +1,26 @@
 	.align 16
 block.18:
-    cmpq $0, %rcx
+    cmpq $0, %rdx
     jg block.20
     jmp block.19
 
 	.align 16
 block.19:
-    movq %rdx, %rdi
+    movq %rcx, %rdi
     callq print_int
     movq $0, %rax
     jmp conclusion
 
 	.align 16
 block.20:
-    addq %rcx, %rdx
-    subq $1, %rcx
+    addq %rdx, %rcx
+    subq $1, %rdx
     jmp block.18
 
 	.align 16
 start:
-    movq $5, %rcx
-    movq $0, %rdx
+    movq $5, %rdx
+    movq $0, %rcx
     jmp block.18
 
 	.globl main
@@ -28,16 +28,16 @@ start:
 main:
     pushq %rbp
     movq %rsp, %rbp
-    pushq %r12
     pushq %r13
+    pushq %r12
     subq $0, %rsp
     jmp start
 
 	.align 16
 conclusion:
     addq $0, %rsp
-    popq %r12
     popq %r13
+    popq %r12
     popq %rbp
     retq
 
