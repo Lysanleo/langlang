ccfe449d52df53356eefbf880b6d2baec6c5562d
Lysanleo <lysanleo347@outlook.com>
2023-08-09 20:55:31 +0800

    Finished remove complex operand pass for Ltup.

--- compiler.py
+++ compiler.py
@@ -70,12 +70,12 @@ class Compiler:
 
     def build_atomic_pair(self,
                           atomic_p:bool,
-                          e:expr,
+                          exp:expr,
                           bindings:Temporaries) -> tuple[expr, Temporaries]:
         if atomic_p:
             new_sym = generate_name("tmp")
-            return (Name(new_sym), bindings + [(Name(new_sym), e)])
-        return (e, bindings)
+            return (Name(new_sym), bindings + [(Name(new_sym), exp)])
+        return (exp, bindings)
 
     def rco_flat(self, rcolist: tuple[expr, Temporaries]) -> List[stmt]:
         stmts = [Assign([atmtp[0]], atmtp[1]) for atmtp in rcolist[1]]
@@ -86,22 +86,20 @@ class Compiler:
         pass
 
     def rco_exp(self, e: expr, need_atomic: bool) -> tuple[expr, Temporaries]:
-        new_sym = ""
         match e:
             case Constant(value):
                 new_expr = e
+                need_atomic = False
                 new_bindings = []
             case Name(id):
-                # return (e, [(e, e)])
                 new_expr = e
+                need_atomic = False
                 new_bindings = []
             case BinOp(latm, Add(), ratm):
                 atm1 = self.rco_exp(latm, True)
                 atm2 = self.rco_exp(ratm, True)
                 new_expr = BinOp(atm1[0], Add(), atm2[0])
                 new_bindings = atm1[1] + atm2[1]
-                # print("In BinOp Case:")
-                # print(new_bindings)
             case BinOp(latm, Sub(), ratm):
                 atm1 = self.rco_exp(latm, True)
                 atm2 = self.rco_exp(ratm, True)
@@ -119,7 +117,7 @@ class Compiler:
                 new_expr = e
                 new_bindings = []
             case Compare(latm, [cmp], [ratm]) as c:
-                print(ratm)
+                # print(ratm)
                 atm1 = self.rco_exp(latm, True)
                 atm2 = self.rco_exp(ratm, True)
                 new_expr = Compare(atm1[0], [cmp], [atm2[0]])
@@ -135,34 +133,30 @@ class Compiler:
                 new_bindings = atm1[1]
         return self.build_atomic_pair(need_atomic, new_expr, new_bindings)
 
-    def rco_stmt(self, s: stmt) -> List[stmt]:
+    def rco_stmt(self, s: stmt) -> list[stmt]:
+        new_stmts = []
         match s:
             case Expr(Call(Name('print'), [atom])):
                 # print(atom)
                 rcotp = self.rco_exp(atom, True)
                 # print(rcotp)
-                new_exprs = self.rco_flat(rcotp)
-                new_exprs.append(Expr(Call(Name('print'), [rcotp[0]])))
-                # print(new_exprs)
-                return new_exprs
+                new_stmts = self.rco_flat(rcotp)
+                new_stmts.append(Expr(Call(Name('print'), [rcotp[0]])))
+                # print(new_stmts)
             case Expr(exp):
                 rcotp = self.rco_exp(exp, False)
-                new_exprs = self.rco_flat(rcotp)
-                new_exprs.append(Expr(rcotp[0]))
-                return new_exprs
+                new_stmts = self.rco_flat(rcotp)
+                new_stmts.append(Expr(rcotp[0]))
             case Assign([Name(var)], exp):
                 rcotp = self.rco_exp(exp, False)
                 # print(rcotp)
-                new_exprs = self.rco_flat(rcotp)
-                new_exprs.append(Assign([Name(var)], rcotp[0]))
-                # print(new_exprs)
-                return new_exprs
+                new_stmts = self.rco_flat(rcotp)
+                new_stmts.append(Assign([Name(var)], rcotp[0]))
+                # print(new_stmts)
             # L_if
             case If(cond, ss1, ss2):
-                rcotp = self.rco_exp(cond, False)
-                new_exprs = self.rco_flat(rcotp)
-                # print(ss1)
-                # print(ss2)
+                new_cond_rcotp = self.rco_exp(cond, False)
+                new_stmts = self.rco_flat(new_cond_rcotp)
                 new_ss1_temp = [self.rco_stmt(stm) for stm in ss1]
                 new_ss2_temp = [self.rco_stmt(stm) for stm in ss2]
                 new_ss1 = []
@@ -173,21 +167,18 @@ class Compiler:
                 for stmts in new_ss2_temp:
                     if stmts:
                         new_ss2 = new_ss2 + stmts
-                # print(new_exprs)
-                # print(rcotp)
-                new_exprs.append(If(rcotp[0], new_ss1, new_ss2))
-                return new_exprs
+                new_stmts.append(If(new_cond_rcotp[0], new_ss1, new_ss2))
             # L_while
             case While(cond, ss1, _):
-                rcotp = self.rco_exp(cond, False)
-                new_exprs = self.rco_flat(rcotp)
+                new_cond_rcotp = self.rco_exp(cond, False)
+                new_stmts = self.rco_flat(new_cond_rcotp)
                 new_ss1_temp = [self.rco_stmt(stm) for stm in ss1]
                 new_ss1 = []
                 for stmts in new_ss1_temp:
                     if stmts:
                         new_ss1 = new_ss1 + stmts
-                new_exprs.append(While(rcotp[0], new_ss1, []))
-                return new_exprs
+                new_stmts.append(While(new_cond_rcotp[0], new_ss1, []))
+        return new_stmts
 
     def remove_complex_operands(self, p: Module) -> Module:
         match p:
